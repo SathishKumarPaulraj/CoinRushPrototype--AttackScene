9705006b0a336f588d24788d41f7b14c54bcb2ac
Sathish <sathishkumar.paulraj1991@gmail.com>
2021-11-19 07:52:01 +0530

    Attack Scene Initial Movement

--- Assets/Prototype/AttackScene3/Scripts/AttackCameraController.cs
+++ Assets/Prototype/AttackScene3/Scripts/AttackCameraController.cs
@@ -14,12 +14,15 @@ public class AttackCameraController : MonoBehaviour
     public bool _CameraFreeRoam = true;
     //  int? i = null;
 
-    public Transform _CameraParent;
-    [Header("Horizontal Panning")]
     //[SerializeField] private Transform mTargetToRotateAround;
     [SerializeField] private float mHorizontalPanSpeed;
     public float _CameraLeftBound = 0;
     public float _CameraRightBound = 0;
+
+    public float _CameraUpBound = 0;
+    public float _CameraDownBound = 0;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +33,11 @@ public class AttackCameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButton(0))
+        {
+            HorizontalPanning();
+            Debug.Log("Working");
+        }
         //staticMovement();
         //  HorizontalPanningWithRotation();
     }
@@ -111,35 +119,22 @@ public class AttackCameraController : MonoBehaviour
         if (!Input.GetMouseButton(0))
         {
             //PlayCameraBoundEffectX();
-            PlayCameraBoundEffect(_CameraParent.position.x, _CameraRightBound, _CameraLeftBound, new Vector3(_CameraLeftBound, _CameraParent.position.y, _CameraParent.position.z), new Vector3(_CameraRightBound, _CameraParent.position.y, _CameraParent.position.z));
+            // PlayCameraBoundEffect(_CameraParent.position.x, _CameraRightBound, _CameraLeftBound, new Vector3(_CameraLeftBound, _CameraParent.position.y, _CameraParent.position.z), new Vector3(_CameraRightBound, _CameraParent.position.y, _CameraParent.position.z));
         }
+
+        //if ((transform.position.x <= _CameraRightBound + 30 && panSpeed > 0) || (transform.position.x >= _CameraLeftBound - 2 && panSpeed < 0))
+        //{
+        //    transform.Translate(panSpeed * transform.right);
+        //}
 
     }
 
     private void Pan(float inPanSpeed) //New Addition
     {
-        if ((_CameraParent.position.x <= _CameraRightBound + 30 && inPanSpeed > 0) || (_CameraParent.position.x >= _CameraLeftBound - 30 && inPanSpeed < 0))
+        if ((transform.position.x <= _CameraRightBound + 30 && inPanSpeed > 0) || (transform.position.x >= _CameraLeftBound - 30 && inPanSpeed < 0))
         {
-            _CameraParent.Translate(inPanSpeed * _CameraParent.right);
+            transform.Translate(inPanSpeed * transform.right);
         }
     }
 
-    public void PlayCameraBoundEffect(float inCameraDirection, float inBound1, float inBound2, Vector3 inCameraBound1, Vector3 inCameraBound2) //Function Modification
-    {
-        Vector3 newCameraParentPos = Vector3.zero;
-        if (inCameraDirection > inBound1 || inCameraDirection < inBound2)
-        {
-
-            if (Mathf.Abs(inBound2 - inCameraDirection) < Mathf.Abs(inBound1 - inCameraDirection))
-            {
-                newCameraParentPos = inCameraBound1;
-            }
-            else
-            {
-                newCameraParentPos = inCameraBound2;
-            }
-
-            _CameraParent.position = Vector3.Lerp(_CameraParent.position, newCameraParentPos, 0.1f);
-        }
-    }
 }
