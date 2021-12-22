032018660154f0f771e79ce72f6b98a8d4d04cae
Sathish <sathishkumar.paulraj1991@gmail.com>
2021-12-22 23:28:32 +0530

    framerate 30

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -40,7 +40,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        Application.targetFrameRate = 60;
+        Application.targetFrameRate = 30;
         if (Instance == null)
         {
             Instance = this;
